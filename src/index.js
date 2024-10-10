8c7a9373d380c97683547d12b0db78991dafd314
Bekzhan Kalzhan <bekzhan.kalzhan@kcell.kz>
2024-10-11 02:28:26 +0500

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -15,9 +15,12 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+.catch(err => {
+    console.error('MongoDB connection error:', err);
+});
 
 // Routes
 app.use('/api', userRoutes);

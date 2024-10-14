9ef14f215873da7419a2545f2fa76948ccf95538
Bekzhan Kalzhan <bekzhan.kalzhan@kcell.kz>
2024-10-14 12:07:45 +0500

    feat: update Truck and TruckDrivers

--- src/models/TruckDriver.js
+++ src/models/TruckDriver.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose');
 
 const TruckDriverSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  licenseNumber: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  surname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /\d{10,15}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  assignedTruckId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Truck',
+    required: true
+  }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('TruckDriver', TruckDriverSchema);
